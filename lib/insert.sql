9a8ff52c239fdfd2e6944704bdf4d9145831c72e
kylewstewart <kyle.w.stewart@mac.com>
2017-04-10 16:56:05 -0400

    Complete SQL-bear

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+
+
+INSERT INTO bears (name, age , breed, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 6, "Grizzly", "M", "Brown", "Angry", 0);
+
+INSERT INTO bears (name, age , breed, gender, color, temperament, alive)
+VALUES ("Rowdy", 6, "Grizzly", "M", "Brown", "Angry", 0);
+
+INSERT INTO bears (name, age , breed, gender, color, temperament, alive)
+VALUES ("Tabitha", 6, "Grizzly", "M", "Brown", "Angry", 0);
+
+INSERT INTO bears (name, age , breed, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 6, "Grizzly", "M", "Brown", "Angry", 0);
+
+INSERT INTO bears (name, age , breed, gender, color, temperament, alive)
+VALUES ("Melissa", 6, "Grizzly", "M", "Brown", "Angry", 0);
+
+INSERT INTO bears (name, age , breed, gender, color, temperament, alive)
+VALUES ("Grinch", 6, "Grizzly", "M", "Brown", "Angry", 0);
+
+INSERT INTO bears (name, age , breed, gender, color, temperament, alive)
+VALUES ("Wendy", 6, "Grizzly", "M", "Brown", "Angry", 0);
+
+INSERT INTO bears (name, age , breed, gender, color, temperament, alive)
+VALUES (NULL, 6, "Grizzly", "M", "Brown", "Angry", 0);
